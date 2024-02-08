3d1dc477f2e73dad83517aeffd12feb275bee727
Enzo D <enzo.danjour@gmail.com>
2024-02-08 20:32:35 -0300

    add escape sequences to print special chars in java

---
+++ SequenciasEscape.java
@@ -0,0 +1,11 @@
+public class SequenciasEscape{
+	
+	public static void main(String[] args){
+		System.out.println("Hi \"Enzo\"");// make quotation marks
+		
+		System.out.println("Your name:\nJohn");//Break new line
+		
+		System.out.println("C:\\Windows");//escape from back slash
+		
+	}
+}
